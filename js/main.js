7c09dc1e95fe324513f9fb0b93e29f4ee6870098
gavhanna <gavhanna@gmail.com>
2016-06-25 23:37:37 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,101 @@
+var breakTime = $("#breakTime").text(),
+  workTime = $("#workTime").text(),
+  breakMinusButton = $("#breakMinus"),
+  breakPlusButton = $("#breakPlus"),
+  workMinusButton = $("#workMinus"),
+  workPlusButton = $("#workPlus"),
+  minutes = workTime,
+  seconds = 0,
+  running = false,
+  counter,
+  audio = new Audio('boop.mp3');
+
+function beep() {
+  audio.play();
+};
+
+breakMinusButton.on("click", function(){
+  if (breakTime > 1){
+    breakTime--;
+    $("#breakTime").text(breakTime);
+  }
+});
+
+breakPlusButton.on("click", function(){
+  if (breakTime > 1){
+    breakTime++;
+    $("#breakTime").text(breakTime);
+  }
+});
+
+workMinusButton.on("click", function(){
+  if (workTime > 1){
+    workTime--;
+    minutes--;
+    $("#workTime").text(workTime);
+    $("#minutes").text(workTime);
+  }
+});
+
+workPlusButton.on("click", function(){
+  if (workTime > 1){
+    workTime++;
+    minutes++;
+    $("#workTime").text(workTime);
+    $("#minutes").text(workTime);
+  }
+});
+
+$("#reset").on("click", function(){
+  breakTime = 5;
+  workTime = 25;
+  $("#breakTime").text(breakTime);
+  $("#workTime").text(workTime);
+  $("#timer").text(workTime + ":00");
+});
+
+// Timer -----------------------------
+
+var countdown = function(){
+  var title = $("#timerCircle h3"),
+      timer = $("#timer");
+
+      if (minutes === 0 && seconds === 1) {
+        beep();
+      }
+
+  if (minutes === 0 && seconds === 0) {
+    if (title.text() === "Work") {
+      title.text("Break");
+      minutes = breakTime;
+      timer.html(minutes + ":0" + seconds)
+    } else if (title.text() === "Break") {
+      title.text("Work");
+      minutes = workTime;
+      timer.html(minutes = ":0" + seconds)
+    }
+  } else {
+    if (seconds === 0) {seconds = 60; minutes--}
+      seconds--;
+      if (seconds < 10) {timer.html(minutes + ":0" + seconds);
+    } else {
+      timer.html(minutes + ":" + seconds);
+    }
+  }
+};
+
+var playPause = function(){
+
+    $("#start").text("Pause");
+    $("#timerCircle h3").text("Work");
+    if (running === false) {
+      counter = setInterval(countdown, 1000);
+      running = true;
+    } else if (running === true) {
+      $("#start").text("Resume");
+      clearInterval(counter);
+      running = false;
+    }
+}
+$("#start").click(playPause);
+$("#timerCircle").click(playPause);
